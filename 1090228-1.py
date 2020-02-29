5716f7895ca18549500b4932ce8106f5886aebb9
Hua Yen <aer456987@gmail.com>
2020-02-29 14:50:00 +0800

    input text

---
+++ 1090228-1.py
@@ -0,0 +1,32 @@
+# input練習_1
+name1 = input("輸入第一個名字: ")
+name2 = input("輸入第二個名字: ")
+now_feel = input("輸入你現在的感覺: ")
+print(name1, "對", name2, "感到", now_feel )
+
+
+print("\n")
+# input練習_2:對話加入input訊息
+print("???:Hello,nice to meet you.")
+print("???:My name is Abel.What is your name?")
+print("You:Hi,nice to meet you too.")
+name = input("You:My name is: ")
+print("Abel:", name, "?")
+print("Abel:Is nice name!")
+
+
+print("\n")
+# input練習_3:基本資料帶入文章中
+name = input("Enter Your name: ")
+birthday = input("Your birthday: ")
+favorite_people = input("What's your favorite people: ")
+day = input("What day do you like: ")
+job = input("Your job is: ")
+print("\n")
+print("The young man stands under the tree on ", day, ".")
+print("his name is ", name, ".")
+print("He is an ", job, ".")
+print("Today is ", birthday, ",it is his birthday.")
+print("So he's waiting for his favorite ", favorite_people, " for dinner ", 
+	  "together!")
+print("He is really excited!")
