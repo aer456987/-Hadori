fea8c4a8121646d37bf3964f1f189548f17046cb
Hua Yen <aer456987@gmail.com>
2020-03-01 00:13:02 +0800

    password_text1

--- 1090229-3-while-password.py
+++ 1090229-3-while-password.py
@@ -1,21 +1,21 @@
-# you_password = "0123456789"
-# x = input("請輸入密碼: ")
-# while True:
-# 	if you_password == x:
-# 		print("歡迎登入。")
-# 		break
-# 	elif you_password != x:
-# 		print("你還有兩次機會。")
-# 		x = input("請輸入密碼: ")
-# 		if you_password == x:
-# 			print("歡迎登入。")
-# 			break
-# 		elif you_password != x:
-# 			print("你還有一次機會。")
-# 			x = input("請輸入密碼: ")
-# 			if you_password == x:
-# 				print("歡迎登入。")
-# 				break
-# 			elif you_password != x:
-# 				print("無緣byebye。")
-# 				break
+you_password = "0123456789"
+x = input("請輸入密碼: ")
+while True:
+	if you_password == x:
+		print("歡迎登入。")
+		break
+	elif you_password != x:
+		print("你還有兩次機會。")
+		x = input("請輸入密碼: ")
+		if you_password == x:
+			print("歡迎登入。")
+			break
+		elif you_password != x:
+			print("你還有一次機會。")
+			x = input("請輸入密碼: ")
+			if you_password == x:
+				print("歡迎登入。")
+				break
+			elif you_password != x:
+				print("無緣byebye。")
+				break
