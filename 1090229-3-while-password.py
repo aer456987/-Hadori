9b6b123ee0dd9d834462bf030962fb33c7f6ef34
Hua Yen <aer456987@gmail.com>
2020-03-01 00:50:00 +0800

    del True,change i>0

--- 1090229-3-while-password.py
+++ 1090229-3-while-password.py
@@ -1,7 +1,7 @@
-#第三次嘗試
+# 第四次嘗試
 you_password = "0123456789"
 i = 3
-while True:
+while i > 0:
 	pwd = input("請輸入密碼: ")
 	if pwd == you_password:
 		print("歡迎登入。")
@@ -9,6 +9,3 @@ while True:
 	else:
 		i = i - 1
 		print("密碼錯誤,你還有", i, "次機會。")
-		if i == 0:
-			print("無緣byebye。")
-			break
