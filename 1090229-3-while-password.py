8133ff8c82ea5c3c49fd89da52de1fa44a7ab428
Hua Yen <aer456987@gmail.com>
2020-03-01 00:37:15 +0800

    Updata_password2

--- 1090229-3-while-password.py
+++ 1090229-3-while-password.py
@@ -1,15 +1,14 @@
+#第三次嘗試
 you_password = "0123456789"
-x = input("請輸入密碼: ")
+i = 3
 while True:
-	if you_password == x:
+	pwd = input("請輸入密碼: ")
+	if pwd == you_password:
 		print("歡迎登入。")
 		break
-	elif you_password != x:
-		print("你還有兩次機會。")
-		x = input("請輸入密碼: ")
-		if you_password != x:
-			print("你還有一次機會。")
-			x = input("請輸入密碼: ")
-			if you_password != x:
-				print("無緣byebye。")
-				break
+	else:
+		i = i - 1
+		print("密碼錯誤,你還有", i, "次機會。")
+		if i == 0:
+			print("無緣byebye。")
+			break
